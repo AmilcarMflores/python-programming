68ab105b9d3baed1fe40ffc32ea683da8a0f6ea7
AmilcarMflores <amilcarmflores@gmail.com>
2025-01-07 02:11:12 -0400

    07/01/2025

---
+++ 12. Looping/12.1.simple.for.py
@@ -0,0 +1,13 @@
+for number in [0, 1, 2, 3, 4]:
+  print(number)
+  
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+# Usando range
+for number in range(5):
+  print(number)
---
+++ 12. Looping/12.3.simple.for.2.py
@@ -0,0 +1,12 @@
+surnames = ["Rivest", "Shamir", "Adleman"]
+for position in range(len(surnames)):
+  #print(position, surnames[position])
+  print(surnames[position][0], end="")
+print()
+
+# Output:
+# 0 Rivest
+# 1 Shamir
+# 2 Adleman
+
+# RSA
